e6994b5f368b5faaf5f2b3e3003c01b0a142a337
Yvonne213 <hywener@gmail.com>
2024-11-02 17:25:18 -0500

    update page 4

--- example1/page3.js
+++ example1/page3.js
@@ -1,2 +1,7 @@
 document.addEventListener('DOMContentLoaded', loadSelectionHistory);
 
+//gotopage 4
+// Function to redirect to page3.html
+function goToPage4() {
+    window.location.href = 'page4.html';
+}
