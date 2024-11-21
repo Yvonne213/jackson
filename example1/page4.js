7e5730d945e92e51eb24c2eed2e34742671a705f
Yvonne213 <hywener@gmail.com>
2024-11-20 21:40:57 -0600

    update

--- example1/page4.js
+++ example1/page4.js
@@ -9,3 +9,18 @@
         alert('Unable to access webcam. Please check your permissions and device settings.');
     }
 })();
+
+
+const form = document.getElementById('info-form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent page reload
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+
+        console.log('Form Submitted:');
+        console.log('Name:', name);
+        console.log('Email:', email);
+
+        alert('Thank you for your submission!');
+    });
